3a603eb1f29ad7d4245327f04a8d7cdb4d283e61
DrewSosa <andrewsosanya@gmail.com>
2023-08-19 21:39:19 -0400

    set up NPM install and env variables script.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+# Set up script for airtable project. Loads environment variables.
+
+export BASE_ID=app8KfPBYY1qdkmJ2
+
+export TOKEN=patz2dsoeadzkgv5j.1f2cedc138eb3f26b89e22930cffa1331ed7e68099928bd07c189f1e183819bc
+export TEST_TOKEN=pat3LWjEMkwP7RsHV.0a1390b22dc5f9e14d2d805103b9d8ff9a1a85bf7bd8b6a5bd3489868b0c5df9
